fee798bfd37a18ff85a1dbd12b0b856bef010624
Rafael Anjos Santos <68437256+YoungC0DE@users.noreply.github.com>
2022-12-04 12:04:52 -0300

    Update users.php

--- api/users.php
+++ api/users.php
@@ -2,13 +2,28 @@
 
 if ($api == 'usuarios') {
     if ($method == 'POST' && $action == 'login') {
-        $nome = array_values($_POST)[0];
+        $email = array_values($_POST)[0];
         $senha = array_values($_POST)[1];
 
         $db = DB::connect();
-        $query = $db->prepare("SELECT * FROM usuarios WHERE email = '$nome' AND senha = '$senha'");
+        $query = $db->prepare("SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'");
         $query->execute();
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            echo json_encode(['dados' => 'Logado com sucesso!']);
+        } else {
+            echo json_encode(['dados' => 'Usuário ou senha incorretos!']);
+        }
+    }
+    else if ($method == 'POST' && $action == 'register') {
+        $nome = array_values($_POST)[0];
+        $email = array_values($_POST)[1];
+        $senha = array_values($_POST)[2];
+
+        $db = DB::connect();
+        $query = $db->prepare("INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email' '$senha')";
+        $result = $query->execute();
 
         if ($result) {
             echo json_encode(['dados' => 'Logado com sucesso!']);
