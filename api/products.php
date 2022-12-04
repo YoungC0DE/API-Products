61ba4f7d221eac5e4cbf8f8d7841487abcc809a7
Rafael Anjos Santos <68437256+YoungC0DE@users.noreply.github.com>
2022-12-04 17:32:14 -0300

    fix

--- api/products.php
+++ api/products.php
@@ -19,12 +19,9 @@ if ($api == 'produtos') {
         $query->execute();
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
-        if ($result) {
-            echo json_encode(['dados' => $result]);
-        } else {
-            echo json_encode(['dados' => 'Nenhum produto encontrado!']);
-        }
-
+        if ($result) echo json_encode(['dados' => $result]);
+        else echo json_encode(['dados' => 'Nenhum produto encontrado!']);
+        
     } else if ($method == 'POST' && $action == 'register') {
         $data = json_decode(file_get_contents('php://input'));
 
@@ -46,23 +43,24 @@ if ($api == 'produtos') {
             $query = $db->prepare("INSERT INTO produtos (fk_usuario, nome, quantidade, medida, valor) VALUES ($fk_usuario, '$nome', $quantidade, '$medida', $valor)");
             $result = $query->execute();
 
-            if ($result) {
-                echo json_encode(['dados' => 'Produtos adicionados com sucesso!']);
-            } else {
-                echo json_encode(['dados' => 'Erro ao adicionar produtos']);
-            }
+            if ($result) echo json_encode(['dados' => 'Produtos adicionados com sucesso!']);
+            else echo json_encode(['dados' => 'Erro ao adicionar produtos']);
         }
 
     } else if ($method == 'DELETE' && $action == 'delete' && $param != '') {
         $db = DB::connect();
+
+        $query = $db->prepare("SELECT id FROM produtos WHERE id = $param");
+        $query->execute();
+        $resultVerify = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        if(!$resultVerify) {echo json_encode(['dados' => 'O produto nao existe']); exit;}
+
         $query = $db->prepare("DELETE FROM produtos WHERE id = $param");
         $result = $query->execute();
 
-        if ($result) {
-            echo json_encode(['dados' => 'Produto deletado!']);
-        } else {
-            echo json_encode(['dados' => 'Erro ao deletar o produtos']);
-        }
+        if ($result)  echo json_encode(['dados' => 'Produto deletado!']);
+        
     } else {
         echo json_encode(['ERRO' => 'Caminho não encontrado']);
     }
--- api/users.php
+++ api/users.php
@@ -27,14 +27,14 @@ if ($api == 'usuarios') {
 
         $db = DB::connect();
 
-        $query = $db->prepare("SELECT * FROM usuarios WHERE email = $email");
+        $query = $db->prepare("SELECT * FROM usuarios WHERE email = '$email'");
         $query->execute();
         $resultVerify = $query->fetchAll(PDO::FETCH_ASSOC);
 
         if ($resultVerify) {
             echo json_encode(['dados' => 'O login já existe!']);
         } else {
-            $query = $db->prepare("INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email' '$senha')");
+            $query = $db->prepare("INSERT INTO usuarios (nome, email, senha) VALUES ('$nome', '$email', '$senha')");
             $result = $query->execute();
 
             if ($result) {
@@ -43,6 +43,45 @@ if ($api == 'usuarios') {
                 echo json_encode(['dados' => 'Erro ao criar login!']);
             }
         }
+        
+    } else if ($method == 'GET' && $action == 'list') {
+        $db = DB::connect();
+
+        $query = $db->prepare("SELECT * FROM usuarios");
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            echo json_encode(['dados' => $result]);
+        } else {
+            echo json_encode(['dados' => 'Nenhum usuario encontrado!']);
+        }
+        
+    } else if ($method == 'DELETE' && $action == 'delete') {
+        $data = json_decode(file_get_contents('php://input'));
+
+        $email = $data -> email;
+        $senha = $data -> senha;
+
+        $db = DB::connect();
+
+        $query = $db->prepare("SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'");
+        $query->execute();
+        $resultVerify = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        if (!$resultVerify) {
+            echo json_encode(['dados' => 'Usuario não existe!']);
+        } else {
+            $query = $db->prepare("DELETE FROM usuarios WHERE email = '$email' and senha = '$senha'");
+            $result = $query->execute();
+
+            if ($result) {
+                echo json_encode(['dados' => 'Conta deletada com sucesso!']);
+            } else {
+                echo json_encode(['dados' => 'Erro ao deletar conta!']);
+            }
+        }
+        
     } else {
         echo json_encode(['ERRO' => 'Caminho não encontrado']);
     }
