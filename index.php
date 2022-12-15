780622fbd28ffb73ad36539cccc3460288d38da1
Rafael Anjos Santos <68437256+YoungC0DE@users.noreply.github.com>
2022-12-15 15:50:53 -0300

    update

--- index.php
+++ index.php
@@ -6,29 +6,15 @@ header('Content-type: application/json');
 
 date_default_timezone_set("America/Sao_Paulo");
 
-if (isset($_GET['path'])) {
-    $path = explode("/", $_GET['path']);
-} else {
+if (!isset($_GET['path']) || isset($path[0])) {
     echo json_encode(['message' => 'Caminho não encontrado']);
     exit;
 }
 
-if (isset($path[0])) {
-    $api = $path[0];
-} else {
-    echo json_encode(['message' => 'Caminho não encontrado']);
-    exit;
-}
-if (isset($path[1])) {
-    $action = $path[1];
-} else {
-    $action = '';
-}
-if (isset($path[2])) {
-    $param = $path[2];
-} else {
-    $param = '';
-}
+$path = explode("/", $_GET['path']);
+$api = isset($path[0]) ? $path[0] : '';
+$action = isset($path[1]) ? $path[1] : '';
+$param = isset($path[2]) ? $path[2] : '';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
