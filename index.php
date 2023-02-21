36d7451715afc37b7a99755412b27d4c4bf588e1
Rafael Anjos Santos <68437256+YoungC0DE@users.noreply.github.com>
2023-02-21 00:20:16 -0300

    fix

--- index.php
+++ index.php
@@ -10,10 +10,20 @@ ini_set('display_errors', 1);
 ini_set('display_startup_erros', 1);
 error_reporting(E_ALL);
 
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    // header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding");
+    exit;
+}
+
 if (!isset($_GET['path'])) {
     http_response_code(404);
     echo json_encode(['message' => 'Not found']);
-    
 }
 // domain/api/action/{param}
 $path = explode("/", $_GET['path']);
@@ -34,7 +44,7 @@ $param = isset($path[3]) ? $path[3] : '';
 include_once "config/connection.php";
 
 if ($api === 'products') include_once "api/products.php";
-if ($api === 'users') include_once "api/users.php";
+if ($api === 'users')    include_once "api/users.php";
 else {
     http_response_code(404);
     echo json_encode(['message' => 'Not found']);
