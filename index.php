9bad2e44d2f506f45632f4369eae04e21a03b670
Rafael Anjos Santos <68437256+YoungC0DE@users.noreply.github.com>
2023-02-21 00:27:28 -0300

    fix

--- index.php
+++ index.php
@@ -12,13 +12,6 @@ error_reporting(E_ALL);
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    // header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding");
-    exit;
 }
 
 if (!isset($_GET['path'])) {
